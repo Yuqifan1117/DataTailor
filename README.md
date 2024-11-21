758f59299cd8834563d408c213fb1e0675943a54
Yuqifan1117 <1138414330@qq.com>
2024-11-22 00:47:48 +0800

    update code of DataTailor

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# DataTailor
+# ✂ DataTailor
+Mastering Collaborative Multi-modal Data Selection: A Focus on *Informativeness*, *Uniqueness*, and *Representativeness*
+# 🌍 Introduction
+ Instruction tuning fine-tunes pre-trained Multi-modal Large Language Models (MLLMs) to handle real-world tasks. However, the rapid expansion of visual instruction datasets introduces data redundancy, leading to excessive computational costs. We propose a collaborative framework, **DataTailor**, which leverages three key principles—--informativeness, uniqueness, and representativeness--—for effective data selection. We argue that a valuable sample should be informative of the task, non-redundant, and represent the sample distribution (i.e., not an outlier). We further propose practical ways to score against each principle, which automatically adapts to a given dataset without tedious hyperparameter tuning. Comprehensive experiments on various benchmarks demonstrate that DataTailor achieves 100.8\% of the performance of full-data fine-tuning with only 15\% of the data, significantly reducing computational costs while maintaining superior results. This exemplifies the ``Less is More" philosophy in MLLM development.
+
+# 💡 Three Core principles
+![image](figures/attribute_00.png)
+- **Informativeness**: a valuable sample should be informative of the task, e.g., If the task is reasoning, describing the differences in movements between skiing and ice skating is more informative than simply showing someone skiing. *In above Figure where each axis (heuristically) represents an orthogonal dimension of task information, points along the diagonal carry more information about the task.*
+- **Uniqueness**: a valuable sample should be distinct from others, exhibiting a large distance from nearby samples to reduce data redundancy (*c.f. Figure near the blue dashed region in the intra-cluster space demonstrate high uniqueness*).
+- **Representativeness**: it should be a typical sample in the data distribution. This prevents selecting noisy outliers or mislabeled samples (c.f. *Figure the clusters connected by blue lines in the inter-cluster space exhibit high representativeness for the overall dataset*). 
+
+# ⭐ Highlight & Results
+![image](figures/intro_performance_00.png)
+- We identify three key principles~(*i.e.*, informativeness, uniqueness, and representativeness) from a systematic perspective to master multi-modal data selection.
+- We propose a unified framework, **DataTailor**, to adaptively integrate these principles for value evaluation to optimize multi-modal data selection in a collaborative way.
+- Extensive results show DataTailor's effectiveness in optimizing all three principles during selection and achieving new SOTA performance on various benchmarks.
+# 🛠️ Setup Steps
+1. Conda a new python environment
+    ```
+    conda create --name datatailor python=3.10
+    conda activate datatailor
+    pip install -r requirements.txt
+    ```
+3. Prepare candidate datasets (1. [MiniGPT4-Instruction](https://github.com/Vision-CAIR/MiniGPT-4/blob/main/dataset/README_2_STAGE.md), 2. [LLaVA-665K](https://huggingface.co/datasets/liuhaotian/LLaVA-Instruct-150K/blob/main/llava_v1_5_mix665k.json), 3. [Mplug-OWL-264K](https://github.com/X-PLUG/mPLUG-Owl/tree/main/mPLUG-Owl), 4. [Bunny-695K](https://huggingface.co/datasets/BoyaWu10/Bunny-v1_0-data/tree/main/finetune))
+4. DataTailor framework
+   1. informativeness
+    ```
+    bash datatailor.sh
+    ```
+   2. cross modal domain clustering
+    ```
+    python make_clustering.py
+    ```
+   3. uniqueness and representativeness
+    ```
+    python relationship_value_calculation.py
+    ```
+   4. collaborative multi-modal data selection
+    ```
+    python datatailor_selector.py
+    ```
+   5. Selected data fine-tuning
+    ```
+    bash finetune_lora_stage2_7b.sh
+    ```
--- make_clustering.py
+++ make_clustering.py
@@ -1,13 +1,11 @@
-from time import time
 import numpy as np
-from scipy.cluster.hierarchy import linkage, fcluster, inconsistent
+import torch
+import math
+from scipy.cluster.hierarchy import fcluster, inconsistent
 from sklearn.datasets import make_blobs
 from sklearn.metrics.cluster import adjusted_mutual_info_score
 import matplotlib.pyplot as plt
-import torch
-import json
-import math
-from datetime import datetime
+from scipy.cluster import _hierarchy
 
 def extract_upper_triangle(distance_matrix_np):
     n = distance_matrix_np.shape[0]
@@ -18,305 +16,40 @@ def extract_upper_triangle(distance_matrix_np):
             pdist_array[idx] = distance_matrix_np[i, j]
             idx += 1
     return pdist_array
-
-def get_posture(x, y, n):
-    return x * n - x * (x + 1) // 2 + y - x - 1
-
-def get_distance(distance_matrix_np):
-    n = distance_matrix_np.shape[0]
-    pdist_array = np.empty((n * (n - 1)) // 2)
-    idx = 0
-    for i in range(n):
-        for j in range(i + 1, n):
-            pdist_array[idx] = distance_matrix_np[i, j]
-            idx += 1
-    return pdist_array
-
-def make_clustering(load_name, save_name):
-    weights = torch.load(load_name)
-    block_number = 30
-
-    weight1 = []
-    weight2 = []
-    num = weights.shape[0]
-    for i in range(0, num):
-        weight1.append(weights[i].double().tolist())
-        weight2.append(weights[i].double())
-
-    weight = torch.tensor(weight1, dtype=torch.float64)
-
-    pdist_np_ = np.empty((num * (num - 1)) // 2)        
-    dist = torch.cdist(weight, weight, p = 2)
-    dist_np = dist.cpu().numpy()
-    pdist_np = extract_upper_triangle(dist_np)
-    Z = linkage(pdist_np, weight2, method='ward')
-    labels_pred = fcluster(Z, t = block_number, criterion = 'maxclust')
-    labels_pred = torch.tensor(labels_pred)
-    print(labels_pred)
-    print(len(labels_pred))
-    torch.save(labels_pred, save_name)
     
-def make_split_clustering(load_name, save_name):
-    weights = torch.load(load_name)
-    st = [0, 57669, 80909]
-    ed = [57668, 80908, 157711]
-    blk_number = [11, 4, 15]
-    labels_pred = []
-    cur_block_number = 0
-    cnt = 3
-    for part in range(0, cnt):
-        weight1 = []
-        weight2 = []
-        num = ed[part] - st[part] + 1
-        block_number = blk_number[part]
-        for i in range(st[part], ed[part] + 1):
-            weight1.append(weights[i].double().tolist())
-            weight2.append(weights[i].double())
+def linkage(pdist_np, y, method='single', metric='euclidean', optimal_ordering=False):
+    n = len(y)
+    result = _hierarchy.nn_chain(pdist_np, n, 5)
+    return result
         
-        weight = torch.tensor(weight1, dtype=torch.float64)
-        
-        pdist_np_ = np.empty((num * (num - 1)) // 2)        
-        dist = torch.cdist(weight, weight, p = 2)
-        dist_np = dist.cpu().numpy()
-        pdist_np = extract_upper_triangle(dist_np)
-        Z = linkage(pdist_np, weight2, method='ward')
-        labels_pred_part = fcluster(Z, t = block_number, criterion = 'maxclust')
-        labels_pred_part = torch.tensor(labels_pred_part) + cur_block_number
-        print(labels_pred_part)
-        if part == 0:
-            labels_pred = labels_pred_part.clone().detach()
-        else:
-            labels_pred = torch.cat((labels_pred, labels_pred_part), dim = 0)
-        cur_block_number += block_number
-    print(labels_pred)
-    print(len(labels_pred))
-    torch.save(labels_pred, save_name)
-
-def make_split_665k_clustering(load_name, save_name):
-    import math
+def make_split_665k_clustering():
+    param = 0.1
     st = [0, 57669, 80909, 157712, 240495, 249493, 315653, 364100, 450517, 522657, 602657, 624610]
-    ed = [62000, 80908, 157711, 240494, 249492, 315652, 364099, 450516, 522656, 602656, 624609, 665297] #57668
+    ed = [57668, 80908, 157711, 240494, 249492, 315652, 364099, 450516, 522656, 602656, 624609, 665297]
     cnt = len(st)
-    blk_number_005 = []
-    blk_number_010 = []
-    blk_number_025 = []
-    blk_number_050 = []
-    blk_number_075 = []
-    blk_number_100 = []
-    
-    cur_block_number_005 = 0
-    cur_block_number_010 = 0
-    cur_block_number_025 = 0
-    cur_block_number_050 = 0
-    cur_block_number_075 = 0
-    cur_block_number_100 = 0
-
-    labels_pred_005 = []
-    labels_pred_010 = []
-    labels_pred_025 = []
-    labels_pred_050 = []
-    labels_pred_075 = []
-    labels_pred_100 = []
-    
+    cur_block_number = 0
+    labels_pred = []
     weights = torch.load("total_global_llava665k_hidden_state.pt")
-
+    print("Start make clustering")
     for part in range(0, cnt):
-        weight1 = []
-        weight2 = []
+        weight = []
+        _weight = []
         num = ed[part] - st[part] + 1
         for i in range(st[part], ed[part] + 1):
-            weight1.append(weights[i].double().tolist())
-            weight2.append(weights[i].double())
-        
-        weight = torch.tensor(weight1, dtype=torch.float64)
-        
-        pdist_np_ = np.empty((num * (num - 1)) // 2)        
-
+            weight.append(weights[i].double().tolist())
+            _weight.append(weights[i].double())
+        weight = torch.tensor(weight, dtype = torch.float64)
+        pdist_np_ = np.empty((num * (num - 1)) // 2)
         dist = torch.cdist(weight, weight, p = 2)
-        
-#        norms = torch.norm(weight, dim=1, keepdim=True)
-#        dot_product = torch.mm(weight, weight.t())
-#        dist = dot_product / (norms * norms.t())
-#        dist = (1 - dist) / 2
-
         dist_np = dist.cpu().numpy()
         pdist_np = extract_upper_triangle(dist_np)
-        Z = linkage(pdist_np, weight2, method='ward')
-        print(Z[len(Z) - 1])
+        Z = linkage(pdist_np, _weight, method='ward')
         distance_threshold = Z[len(Z) - 1][2]
-        print(distance_threshold)  
-        labels_pred_part_050 = torch.tensor(fcluster(Z, t=distance_threshold * 0.50, criterion='distance'))
-        for w in range(0, 10):
-            print((labels_pred_part_050 == w).sum().item())
-            
-        Z = linkage(pdist_np, weight2, method='single')
-        print(Z[len(Z) - 1])
-        distance_threshold = Z[len(Z) - 1][2]
-        print(distance_threshold)  
-        labels_pred_part_050 = torch.tensor(fcluster(Z, t=distance_threshold * 0.50, criterion='distance')) 
-        for w in range(0, 10):
-            print((labels_pred_part_050 == w).sum().item())
+        labels_pred_part = torch.tensor(fcluster(Z, t=distance_threshold * param, criterion='distance')) + cur_block_number
+        cur_block_number = torch.max(labels_pred_part).item()
+        labels_pred.append(labels_pred_part)
 
-        Z = linkage(pdist_np, weight2, method='complete')
-        print(Z[len(Z) - 1])
-        distance_threshold = Z[len(Z) - 1][2]
-        print(distance_threshold)  
-        labels_pred_part_050 = torch.tensor(fcluster(Z, t=distance_threshold * 0.50, criterion='distance')) 
-        for w in range(0, 10):
-            print((labels_pred_part_050 == w).sum().item())
-
-        Z = linkage(pdist_np, weight2, method='average')
-        print(Z[len(Z) - 1])
-        distance_threshold = Z[len(Z) - 1][2]
-        print(distance_threshold)  
-        labels_pred_part_050 = torch.tensor(fcluster(Z, t=distance_threshold * 0.50, criterion='distance')) 
-        for w in range(0, 10):
-            print((labels_pred_part_050 == w).sum().item())
-
-        Z = linkage(pdist_np, weight2, method='weighted')
-        print(Z[len(Z) - 1])
-        distance_threshold = Z[len(Z) - 1][2]
-        print(distance_threshold)  
-        labels_pred_part_050 = torch.tensor(fcluster(Z, t=distance_threshold * 0.50, criterion='distance')) 
-        for w in range(0, 10):
-            print((labels_pred_part_050 == w).sum().item())
-
-        Z = linkage(pdist_np, weight2, method='median')
-        print(Z[len(Z) - 1])
-        distance_threshold = Z[len(Z) - 1][2]
-        print(distance_threshold)  
-        labels_pred_part_050 = torch.tensor(fcluster(Z, t=distance_threshold * 0.50, criterion='distance')) 
-        for w in range(0, 10):
-            print((labels_pred_part_050 == w).sum().item())
-
-#        labels_pred_part_005 = torch.tensor(fcluster(Z, t=distance_threshold * 0.05, criterion='distance')) + cur_block_number_005
-#        cur_block_number_005 = torch.max(labels_pred_part_005).item()
-#        blk_number_005.append(cur_block_number_005)
-
-#        labels_pred_part_010 = torch.tensor(fcluster(Z, t=distance_threshold * 0.10, criterion='distance')) + cur_block_number_010
-#        cur_block_number_010 = torch.max(labels_pred_part_010).item()
-#        blk_number_010.append(cur_block_number_010)
-
-#        labels_pred_part_025 = torch.tensor(fcluster(Z, t=distance_threshold * 0.25, criterion='distance')) + cur_block_number_025
-#        cur_block_number_025 = torch.max(labels_pred_part_025).item()
-#        blk_number_025.append(cur_block_number_025)
-
-
-#        labels_pred_part_075 = torch.tensor(fcluster(Z, t=distance_threshold * 0.75, criterion='distance')) + cur_block_number_075
-#        cur_block_number_075 = torch.max(labels_pred_part_075).item()
-#        blk_number_075.append(cur_block_number_075)
-
-#        labels_pred_part_100 = torch.tensor(fcluster(Z, t=distance_threshold * 1.0, criterion='distance')) + cur_block_number_100
-#        cur_block_number_100 = torch.max(labels_pred_part_100).item()
-#        blk_number_100.append(cur_block_number_100)
-
-
-        torch.save(labels_pred_part_005, "check0050.pt")
-        torch.save(labels_pred_part_010, "check0100.pt")
-        torch.save(labels_pred_part_025, "check0250.pt")
-        torch.save(labels_pred_part_050, "check0500.pt")
-        torch.save(labels_pred_part_075, "check0750.pt")
-        torch.save(labels_pred_part_100, "check1000.pt")
-        exit(0)
-
-        labels_pred_005.append(labels_pred_part_005)
-        labels_pred_010.append(labels_pred_part_010)
-        labels_pred_025.append(labels_pred_part_025)
-        labels_pred_050.append(labels_pred_part_050)
-        labels_pred_075.append(labels_pred_part_075)
-        labels_pred_100.append(labels_pred_part_100)
-        print(labels_pred_005)
-        print(labels_pred_010)
-        print(labels_pred_025)
-        print(labels_pred_050)
-        print(labels_pred_075)
-        print(labels_pred_100)
-    print(labels_pred_005)
-    print(labels_pred_010)
-    print(labels_pred_025)
-    print(labels_pred_050)
-    print(labels_pred_075)
-    print(labels_pred_100)
-    torch.save(labels_pred_005, "llabel_0.05_dis.pt")
-    torch.save(labels_pred_010, "llabel_0.1_dis.pt")
-    torch.save(labels_pred_025, "llabel_0.25_dis.pt")
-    torch.save(labels_pred_050, "llabel_0.5_dis.pt")
-    torch.save(labels_pred_075, "llabel_0.75_dis.pt")
-    torch.save(labels_pred_100, "llabel_1.0_dis.pt")
-    
-    torch.save(blk_number_005, "bblk_number_005.pt")
-    torch.save(blk_number_010, "bblk_number_010.pt")
-    torch.save(blk_number_025, "bblk_number_025.pt")
-    torch.save(blk_number_050, "bblk_number_050.pt")
-    torch.save(blk_number_075, "bblk_number_075.pt")
-    torch.save(blk_number_100, "bblk_number_100.pt")
-
-def make_one_clustering(load_name, save_name):
-    weights = torch.load(load_name)
-    st = [0]
-    ed = [157711]
-    blk_number = [30]
-    labels_pred = []
-    cur_block_number = 0
-    cnt = 1
-    for part in range(0, cnt):
-        for i in range(st[part], ed[part] + 1):
-            labels_pred.append(part + 1)
-    labels_pred = torch.tensor(labels_pred)
-    print(labels_pred)
-    print(len(labels_pred))
-    torch.save(labels_pred, save_name)
-
-def make_three_clustering(load_name, save_name):
-    weights = torch.load(load_name)
-    st = [0, 57669, 80909]
-    ed = [57668, 80908, 157711]
-    blk_number = [11, 4, 15]
-    labels_pred = []
-    cur_block_number = 0
-    cnt = 3
-    for part in range(0, cnt):
-        for i in range(st[part], ed[part] + 1):
-            labels_pred.append(part + 1)
-    labels_pred = torch.tensor(labels_pred)
-    print(labels_pred)
-    print(len(labels_pred))
-    torch.save(labels_pred, save_name)
-
-def make():
-    load_name = ["total_global_llava158k_hidden_state.pt"]
-    save_name = ["total_clustering_global_llava158k_hidden_state.pt"]
-    for i in range(0, len(save_name)):
-        make_clustering(load_name[i], save_name[i])
-
-def make_split():
-    load_name = ["total_global_llava158k_hidden_state.pt"]
-    save_name = ["total_clustering_global_llava158k_hidden_state_split.pt"]
-    for i in range(0, len(save_name)):
-        make_split_clustering(load_name[i], save_name[i])
-
-def make_one():
-    load_name = ["total_global_llava158k_hidden_state.pt"]
-    save_name = ["total_clustering_global_llava158k_hidden_state_one.pt"]
-    for i in range(0, len(save_name)):
-        make_one_clustering(load_name[i], save_name[i])
-    
-def make_three():
-    load_name = ["total_global_llava158k_hidden_state.pt"]
-    save_name = ["total_clustering_global_llava158k_hidden_state_three.pt"]
-    for i in range(0, len(save_name)):
-        make_three_clustering(load_name[i], save_name[i])
-
-def make_665k_split():
-    load_name = ["logits.pt"]
-    save_name = ["total_clustering_global_llava665k_hidden_state_split_1000.pt"]
-    for i in range(0, len(save_name)):
-        make_split_665k_clustering(load_name[i], save_name[i])
+    torch.save(labels_pred, "labels_pred.pt")
 
 if __name__ == '__main__':
-
-#    make()
-#    make_split()
-#    make_one()
-#    make_three()
-    make_665k_split()
+    make_split_665k_clustering()
